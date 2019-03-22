15c721d68d98fa7f405bf2a97e23901155ab1aa8
Eric George <geric738@aol.com>
2019-03-22 13:07:49 -0700

    added get data endpoint in full

--- server/server.js
+++ server/server.js
@@ -1,33 +1,36 @@
 const express = require("express"); //loads the express library into the file
-const mysql = require("mysql");
+const mysql = require("mysql"); //loads the mysql library into the file 
 const mysqlcredentials = require("./mysqlcreds.js");
-const db = mysql.createConnection(mysqlcredentials); //"use these credentials to connect to the database"
+const db = mysql.createConnection(mysqlcredentials); //"use these credentials to establish preliminary connection to the database"
 
 const server = express(); //instantiates a server into the const variable server 
-
 server.use(express.static(__dirname + "/html"));
+server.use(express.urlencoded({extended: false})); //have express pull urlencoded data from the body and store it in 'body'
 
 server.get("/api/grades", (request, response)=>{
-    response.send(`{
-        "success": true,
-        "data": [{
-            "id": 10,
-            "name": "Eric George",
-            "course": "Linear Algebra",
-            "grade": 85
-        }, {
-            "id": 12,
-            "name": "Sandy Happyfeet",
-            "course": "Penguin Dancing",
-            "grade": 80
-        }, {
-            "id": 14,
-            "name": "Chewie Bacca",
-            "course": "Porg Roasting",
-            "grade": 89
-        }]
-    }`);
+    db.connect(()=>{
+        const query = "SELECT `id`, CONCAT(`givenname`, ' ', `surname`) AS `name`, `course`, `grade` FROM `grades`";
+        db.query(query, (error, data)=>{ //sends the query to the database (error=null if there was no error)
+            const output = {
+                success: false,
+            };
+            if (!error){
+                output.success = true;
+                output.data = data;
+            } else {
+                output.error = error;
+            }
+            response.send(output);
+        });
+    });
 });
+//in reality, we wouldn't send the error data back to the client...only during development for our purposes 
+
+server.post("/api/grades", (request, response)=>{
+
+}); //for adding data to the database
+
+
 
 server.listen(3001, ()=>{
     console.log("carrier has arrived");
